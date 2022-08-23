826c2f709a19bb6af7d78a4e24cb0e098c252555
Guuuuh <gustavo.htl@hotmail.com>
2022-08-22 22:03:44 -0300

    Tipo Serviço criado

--- Projeto-HelpCar/src/model/tipoServico.java
+++ Projeto-HelpCar/src/model/tipoServico.java
@@ -1,9 +1,46 @@
 package model;
 
-/**
- *
- * @author gusta
- */
-public class tipoServico {
+public class TipoServico {
+    private int codTipoServico;
+    private int codEmpresa;
+    private String descricao;
+
+    public TipoServico() {
+    }
+
+    public TipoServico(int codTipoServico, int codEmpresa, String descricao) {
+        this.codTipoServico = codTipoServico;
+        this.codEmpresa = codEmpresa;
+        this.descricao = descricao;
+    }
+
+    public int getCodTipoServico() {
+        return codTipoServico;
+    }
+
+    public void setCodTipoServico(int codTipoServico) {
+        this.codTipoServico = codTipoServico;
+    }
+
+    public int getCodEmpresa() {
+        return codEmpresa;
+    }
+
+    public void setCodEmpresa(int codEmpresa) {
+        this.codEmpresa = codEmpresa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "tipoServico{" + "codTipoServico=" + codTipoServico + ", codEmpresa=" + codEmpresa + ", descricao=" + descricao + '}';
+    }
     
 }
