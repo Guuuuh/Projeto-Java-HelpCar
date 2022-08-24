ee410a63862536f0dee76ddf75dc6c5ff09418f9
gabrielvitoretti <gabriel.vitoretti@outlook.com>
2022-08-24 18:25:14 -0300

    Adição dos métodos get, set e toString na classe Veiculo.

--- Projeto-HelpCar/src/model/Veiculo.java
+++ Projeto-HelpCar/src/model/Veiculo.java
@@ -22,4 +22,58 @@ public class Veiculo {
         this.ano = ano; 
         this.placa = placa;
     }
+
+    public int getCodVeiculo() {
+        return codVeiculo;
+    }
+
+    public void setCodVeiculo(int codVeiculo) {
+        this.codVeiculo = codVeiculo;
+    }
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" + "codVeiculo=" + codVeiculo + ", codCliente=" + codCliente + ", marca=" + marca + ", modelo=" + modelo + ", ano=" + ano + ", placa=" + placa + '}';
+    }
+    
 }
