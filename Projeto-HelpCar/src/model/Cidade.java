cf5bc117432d3d19e7e47d94894c42b316d35b5c
N / <101023585+ntxtheprime@users.noreply.github.com>
2022-08-25 20:37:05 -0300

    Adição
    Adição de atributos, construtor, get/set e tostring.

--- Projeto-HelpCar/src/model/Cidade.java
+++ Projeto-HelpCar/src/model/Cidade.java
@@ -1,5 +1,47 @@
 package model;
 
 public class Cidade {
+    
+    private int codCidade;
+    private Estado estado;
+    private String nome;
+    
+        public Cidade(){
+    }
 
+    public Cidade(int codCidade, Estado estado, String nome) {
+        this.codCidade = codCidade;
+        this.estado = estado;
+        this.nome = nome;
+    }
+
+    public int getCodCidade() {
+        return codCidade;
+    }
+
+    public void setCodCidade(int codCidade) {
+        this.codCidade = codCidade;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" + "codCidade=" + codCidade + ", estado=" + estado + ", nome=" + nome + '}';
+    }
+    
 }
