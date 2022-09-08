c883e01bc5ba1c7fa826f04e115eba988386bc24
ggoulartec <zgoularte_@hotmail.com>
2022-09-07 22:22:19 -0300

    atributos, construtor, getters e setters e tostring()

--- Projeto-HelpCar/src/model/Bairro.java
+++ Projeto-HelpCar/src/model/Bairro.java
@@ -1,7 +1,48 @@
 package model;
 
 public class Bairro {
-  private int codBairro;
-  private Cidade cidade;
-  private String nome;
+
+    private int codBairro;
+    private Cidade cidade;
+    private String nome;
+
+    public Bairro() {
+
+    }
+
+    public Bairro(int codBairro, Cidade cidade, String nome) {
+        this.codBairro = codBairro;
+        this.cidade = cidade;
+        this.nome = nome;
+    }
+
+    public int getCodBairro() {
+        return codBairro;
+    }
+
+    public void setCodBairro(int codBairro) {
+        this.codBairro = codBairro;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Bairro{" + "codBairro=" + codBairro + ", cidade=" + cidade + ", nome=" + nome + '}';
+    }
+
 }
